dd7f9aeed9c840d2b79f40102a1344da08e7a763
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-21 14:03:56 -0600

    added reps and sessions controllers

---
+++ app/controllers/reps_controller.rb
@@ -0,0 +1,9 @@
+class RepsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'sessions#new'
+
+  resources :reps
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
