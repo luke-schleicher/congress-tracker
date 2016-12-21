86724a27d2a1663dd992e9e73f3fa41fe609c511
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-21 17:15:35 -0600

    parsing sunlight data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# read from current legislators file and populate database with it
+
+
+def build_url(query)
+  ENDPOINT + query
+end
+
+ENDPOINT = "https://congress.api.sunlightfoundation.com/"
+NUMBER_OF_LEGISLATOR_PAGES = 27
+legislators = "legislators?page="
+legislator_data = []
+
+NUMBER_OF_LEGISLATOR_PAGES.times do |page|
+  legis_page = legislators + (page + 1).to_s
+  url = build_url(legis_page)
+  response = HTTParty.get(url)
+  legislator_data << response
+end
+
+legislator_data[0]["results"]
+
+legislator_data.each do |page|
+
+
+end
+
+
+
+
+
+
