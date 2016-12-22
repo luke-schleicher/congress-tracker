060b5be6cef964a002e2174fd593553110a239c6
Kenneth Kottenstette <kennykotten@gmail.com>
2016-12-21 19:18:16 -0800

    get missing votesmart ids in the seeds file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+gem "figaro"
+gem 'patron', '>=0.4.6'
+gem 'votesmart'
+
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# read from current legislators file and populate database with it
+VoteSmart.api_key = ENV["secret_key"]
 
 def build_url(query)
   ENDPOINT + query
+end
+
+def get_votesmart_id(sunset_object)
+  sunset_state = sunset_object["state"]
+  sunset_lastname = sunset_object["last_name"]
+
+  api_call = VoteSmart::Official.get_by_lastname(sunset_lastname)
+  matching_candidates = api_call["candidateList"]["candidate"]
+
+  matching_candidates.each do |candidate|
+    if candidate["officeStateId"] == sunset_state
+      return candidate["candidateId"]
+    end
+  end
 end
 
 ENDPOINT = "https://congress.api.sunlightfoundation.com/"
@@ -36,11 +42,17 @@ legislator_data.each.with_index do |page, i|
     l.last_name = legislator["last_name"]
     l.gender = legislator["gender"]
     l.email = legislator["oc_email"]
-    l.votesmart_id = legislator["votesmart_id"]
     l.website = legislator["website"]
     l.term_start = legislator["term_start"]
     l.term_end = legislator["term_end"]
     l.state = legislator["state"]
+
+    if legislator["votesmart_id"]
+      l.votesmart_id = legislator["votesmart_id"]
+      else
+        l.votesmart_id = get_votesmart_id(legislator)
+    end
+
     l.phone = legislator["phone"]
     l.office = legislator["office"]
     l.birthday = legislator["birthday"]
@@ -79,6 +91,3 @@ bill_data.each.with_index do |page, i|
   end
   puts "Compiled bill data from page #{i} of Sunlight's database."
 end
-
-
-
