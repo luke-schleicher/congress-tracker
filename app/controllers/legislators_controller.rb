11fb76189e04a402866819a75eef258b18b1d71e
James Harris <computersherpa@gmail.com>
2016-12-22 18:05:30 -0600

    Basic bills

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -17,5 +17,7 @@ class LegislatorsController < ApplicationController
 
   def show
     @legislator = Legislator.find(params[:bioguide_id])
+    @bills = @legislator.bills
+    @first_bill_date = Bill.order(last_vote_at: :asc).first.last_vote_at
   end
 end
