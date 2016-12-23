554271297da9546bebeee5552d6ce194e0e2def6
James Harris <computersherpa@gmail.com>
2016-12-22 18:38:07 -0600

    Conditional bill display

--- app/helpers/legislators_helper.rb
+++ app/helpers/legislators_helper.rb
@@ -1,2 +1,11 @@
 module LegislatorsHelper
+  def colored_yea_nay(vote_value)
+    if vote_value == "Yea"
+      "<strong class='text-success'>Yea:</strong>".html_safe
+    elsif vote_value == "Nay"
+      "<strong class='text-danger'>Nay:</strong>".html_safe
+    else
+      "<strong>#{vote_value}:</strong>".html_safe
+    end
+  end
 end
