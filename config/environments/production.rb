8ecd838591cd33c1ae40ed5e7720cb21c3eabec7
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-23 01:21:21 -0600

    mailer mod to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,18 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+            :address        => 'smtp.sendgrid.net',
+            :port           => '587',
+            :authentication => :plain,
+            :user_name      => ENV['SENDGRID_USERNAME'],
+            :password       => ENV['SENDGRID_PASSWORD'],
+            :domain         => 'heroku.com'
+    }
+    config.action_mailer.default_url_options = { :host => 'ancient-everglades-28054.herokuapp.com' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
