2b0132ff8f35d8cb2b3fd127d1b3095116a4c51b
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-23 12:09:35 -0600

    last seeds update to seed production app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 puts "Removing records"
 
 
-# Legislator.destroy_all
+Legislator.destroy_all
 # Bill.destroy_all
 # Vote.destroy_all
 
@@ -31,7 +31,7 @@ ENDPOINT = "https://congress.api.sunlightfoundation.com/"
 
 puts "Getting legislators..."
 
-NUMBER_OF_LEGISLATOR_PAGES = 0 # 27
+NUMBER_OF_LEGISLATOR_PAGES = 27 # 27
 legislators = "legislators?fields=bioguide_id,fax,first_name,last_name,birthday,chamber,gender,oc_email,party,phone,state,term_end,term_start,website,twitter_id,facebook_id,youtube_id,votesmart_id,crp_id,office&page="
 legislator_data = []
 
@@ -103,100 +103,26 @@ puts "Legislators... created"
 # Bills
 ########################
 
-NUMBER_OF_BILL_PAGES = 0 #20
-bills = "bills?congress=114&fields=bill_id,official_title,popular_title,summary_short,introduced_on,sponsor_id,last_vote_at,keywords,history.enacted&order=history.enacted&per_page=50&page="
-bill_data = []
-
-NUMBER_OF_BILL_PAGES.times do |page|
-  bill_page = bills + (page + 1).to_s
-  url = build_url(bill_page)
-  response = HTTParty.get(url)
-  bill_data << response
-end
-
-puts "Getting bills..."
-
-already_been_dun = []
-bill_data.each.with_index do |page, i|
-  page["results"].each do |bill|
-    b = Bill.new
-
-    b.bill_id = bill["bill_id"]
-
-    b.official_title = bill["official_title"]
-    b.popular_title = bill["popular_title"]
-    b.summary_short = bill["summary_short"]
-    b.introduced_on = bill["introduced_on"]
-    b.last_vote_at = bill["last_vote_at"]
-    b.sponsor_id = bill["sponsor_id"]
-    b.keywords = bill["keywords"]
-    b.enacted = bill["history"]["enacted"]
-    b.save unless already_been_dun.include?(bill["bill_id"])
-    already_been_dun << bill["bill_id"]
-  end
-  puts "Compiled bill data from page #{i} of Sunlight's database."
-end
-
-puts "Finished adding bills to database"
-
-########################
-# Votes
-########################
-
-
-NUMBER_OF_VOTE_PAGES = 0 # 92
-
-votes = "votes?congress=114&fields=voter_ids,bill_id,voted_at&page="
-vote_data = []
-
-NUMBER_OF_VOTE_PAGES.times do |page|
-  vote_page = votes + (page + 1).to_s
-  url = build_url(vote_page)
-  response = HTTParty.get(url)
-  vote_data << response
-end
-
-puts "Getting votes..."
-
-vote_data.each.with_index do |page, i|
-  page["results"].each do |vote|
-
-    vote["voter_ids"].each do |voter, type|
-      v = Vote.new
-      v.bill_id = vote["bill_id"]
-      v.voted_at = vote["voted_at"]
-      v.voter_id = voter
-      v.vote_type = type
-      v.save
-
-    end
-  end
-  puts "Compiled vote data from page #{i} of Sunlight's database."
-end
-
-puts "Finished compiling vote data"
-
-#
-# NUMBER_OF_BILL_PAGES = 20 #20
+# NUMBER_OF_BILL_PAGES = 0 #20
 # bills = "bills?congress=114&fields=bill_id,official_title,popular_title,summary_short,introduced_on,sponsor_id,last_vote_at,keywords,history.enacted&order=history.enacted&per_page=50&page="
 # bill_data = []
-#
+
 # NUMBER_OF_BILL_PAGES.times do |page|
 #   bill_page = bills + (page + 1).to_s
 #   url = build_url(bill_page)
 #   response = HTTParty.get(url)
 #   bill_data << response
 # end
-#
+
 # puts "Getting bills..."
-#
+
 # already_been_dun = []
 # bill_data.each.with_index do |page, i|
 #   page["results"].each do |bill|
 #     b = Bill.new
-#
+
 #     b.bill_id = bill["bill_id"]
-#
+
 #     b.official_title = bill["official_title"]
 #     b.popular_title = bill["popular_title"]
 #     b.summary_short = bill["summary_short"]
@@ -210,31 +136,31 @@ puts "Finished compiling vote data"
 #   end
 #   puts "Compiled bill data from page #{i} of Sunlight's database."
 # end
-#
+
 # puts "Finished adding bills to database"
-#
-# ########################
-# # Votes
-# ########################
-#
-#
-# NUMBER_OF_VOTE_PAGES = 5 # 92
-#
+
+########################
+# Votes
+########################
+
+
+# NUMBER_OF_VOTE_PAGES = 0 # 92
+
 # votes = "votes?congress=114&fields=voter_ids,bill_id,voted_at&page="
 # vote_data = []
-#
+
 # NUMBER_OF_VOTE_PAGES.times do |page|
 #   vote_page = votes + (page + 1).to_s
 #   url = build_url(vote_page)
 #   response = HTTParty.get(url)
 #   vote_data << response
 # end
-#
+
 # puts "Getting votes..."
-#
+
 # vote_data.each.with_index do |page, i|
 #   page["results"].each do |vote|
-#
+
 #     vote["voter_ids"].each do |voter, type|
 #       v = Vote.new
 #       v.bill_id = vote["bill_id"]
@@ -242,10 +168,12 @@ puts "Finished compiling vote data"
 #       v.voter_id = voter
 #       v.vote_type = type
 #       v.save
-#
+
 #     end
 #   end
 #   puts "Compiled vote data from page #{i} of Sunlight's database."
 # end
-#
+
 # puts "Finished compiling vote data"
+
+
