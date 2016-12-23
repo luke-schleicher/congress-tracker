7389e7246616ff5b82f10263fcc4ec10f0211b6a
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-23 03:21:13 -0600

    preparing seeds and tweaked email

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 
 puts "Removing records"
 
-# Legislator.destroy_all
+Legislator.destroy_all
 Bill.destroy_all
 Vote.destroy_all
 
@@ -28,71 +28,71 @@ ENDPOINT = "https://congress.api.sunlightfoundation.com/"
 # Legislators
 ########################
 
-# puts "Getting legislators..."
+puts "Getting legislators..."
 
-# NUMBER_OF_LEGISLATOR_PAGES = 0 # 27
-# legislators = "legislators?fields=bioguide_id,fax,first_name,last_name,birthday,chamber,gender,oc_email,party,phone,state,term_end,term_start,website,twitter_id,facebook_id,youtube_id,votesmart_id,office&page="
-# legislator_data = []
+NUMBER_OF_LEGISLATOR_PAGES = 27 # 27
+legislators = "legislators?fields=bioguide_id,fax,first_name,last_name,birthday,chamber,gender,oc_email,party,phone,state,term_end,term_start,website,twitter_id,facebook_id,youtube_id,votesmart_id,office&page="
+legislator_data = []
 
-# NUMBER_OF_LEGISLATOR_PAGES.times do |page|
-#   legis_page = legislators + (page + 1).to_s
-#   url = build_url(legis_page)
-#   response = HTTParty.get(url)
-#   legislator_data << response
-# end
+NUMBER_OF_LEGISLATOR_PAGES.times do |page|
+  legis_page = legislators + (page + 1).to_s
+  url = build_url(legis_page)
+  response = HTTParty.get(url)
+  legislator_data << response
+end
 
-# legislator_data.each.with_index do |page, i|
-#   page["results"].each do |legislator|
-#     l = Legislator.new
-#     l.fax = legislator["fax"]
-#     l.twitter_url = "https://twitter.com/" + legislator["twitter_id"].to_s
-#     l.youtube_url = "https://youtube.com/" + legislator["youtube_id"].to_s
-#     l.facebook_url = "https://facebook.com/" + legislator["facebook_id"].to_s
-#     l.chamber = legislator["chamber"]
-#     l.party = legislator["party"]
-#     l.first_name = legislator["first_name"]
-#     l.last_name = legislator["last_name"]
-#     l.gender = legislator["gender"]
-#     l.email = legislator["oc_email"]
-#     l.website = legislator["website"]
-#     l.term_start = legislator["term_start"]
-#     l.term_end = legislator["term_end"]
-#     l.state = legislator["state"]
+legislator_data.each.with_index do |page, i|
+  page["results"].each do |legislator|
+    l = Legislator.new
+    l.fax = legislator["fax"]
+    l.twitter_url = "https://twitter.com/" + legislator["twitter_id"].to_s
+    l.youtube_url = "https://youtube.com/" + legislator["youtube_id"].to_s
+    l.facebook_url = "https://facebook.com/" + legislator["facebook_id"].to_s
+    l.chamber = legislator["chamber"]
+    l.party = legislator["party"]
+    l.first_name = legislator["first_name"]
+    l.last_name = legislator["last_name"]
+    l.gender = legislator["gender"]
+    l.email = legislator["oc_email"]
+    l.website = legislator["website"]
+    l.term_start = legislator["term_start"]
+    l.term_end = legislator["term_end"]
+    l.state = legislator["state"]
 
-#     sunset_state = legislator["state"]
+    sunset_state = legislator["state"]
 
-#     sunset_votesmart_id = legislator["votesmart_id"].to_s
+    sunset_votesmart_id = legislator["votesmart_id"].to_s
 
-#     if sunset_votesmart_id.empty?
-#       api_call = VoteSmart::Official.get_by_lastname(legislator["last_name"])
+    if sunset_votesmart_id.empty?
+      api_call = VoteSmart::Official.get_by_lastname(legislator["last_name"])
 
-#       if api_call.to_s.include?("error")
-#         sunset_votesmart_id = "N/A"
-#       else
-#         matching_candidates = api_call["candidateList"]["candidate"]
+      if api_call.to_s.include?("error")
+        sunset_votesmart_id = "N/A"
+      else
+        matching_candidates = api_call["candidateList"]["candidate"]
 
-#         if Array === matching_candidates
-#           matching_candidates.each do |candidate|
-#             sunset_votesmart_id = candidate["candidateId"] if candidate["officeStateId"] == sunset_state
-#           end
-#         elsif Hash === matching_candidates #If only one candidate, it'll be a hash instead
-#           sunset_votesmart_id =  api_call["candidateList"]["candidate"]["candidateId"]
-#         end
-#       end
-#     end
+        if Array === matching_candidates
+          matching_candidates.each do |candidate|
+            sunset_votesmart_id = candidate["candidateId"] if candidate["officeStateId"] == sunset_state
+          end
+        elsif Hash === matching_candidates #If only one candidate, it'll be a hash instead
+          sunset_votesmart_id =  api_call["candidateList"]["candidate"]["candidateId"]
+        end
+      end
+    end
 
-#     l.votesmart_id = sunset_votesmart_id.to_s
+    l.votesmart_id = sunset_votesmart_id.to_s
 
-#     l.phone = legislator["phone"]
-#     l.office = legislator["office"]
-#     l.birthday = legislator["birthday"]
-#     l.bioguide_id = legislator["bioguide_id"]
-#     l.save
+    l.phone = legislator["phone"]
+    l.office = legislator["office"]
+    l.birthday = legislator["birthday"]
+    l.bioguide_id = legislator["bioguide_id"]
+    l.save
 
-#   end
-# end
+  end
+end
 
-# puts "Legislators... created"
+puts "Legislators... created"
 
 
 ########################
